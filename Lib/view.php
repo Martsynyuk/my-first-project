d2e278dff65b4ab321896fc6763ada4f7973bb08
Martsynyuk <terr83@gmail.com>
2015-11-30 16:34:30 +0200

    url_bilder / test version

--- Lib/view.php
+++ Lib/view.php
@@ -28,5 +28,71 @@ Class View
 		echo ob_get_clean();
 		
 	}
+	
+		/**
+		* 	$argument = array(
+		*		
+		*			'controller' => 'contacts',
+		*			'action' => 'index',
+		*			'page' => '1',
+		*			'sort' => 'first',
+		*			'sorparam' => 'asc',
+		*			'id' => '2',
+		*			'all' => 'all'
+		*		);
+		* 
+	 	* 
+	 	* @param array $argument
+	 	* @return string*/
+	
+	public function url_bilder($argument)
+	{
+		
+		$controller = NULL;
+		$action = NULL;
+		$page = NULL;
+		$sort = NULL;
+		$sortparam = NULL;
+		$id = NULL;
+		$all = NULL;
+		
+		foreach ($argument as $key=>$val)
+		{
+			switch ($key)
+			{
+				case 'controller' :
+					$controller = '/' . $val;
+					break;
+				case 'action' :
+					$action = '/' . $val;
+					break;
+				case 'page':
+					$page = '/' . $val;
+					break;
+				case 'sort':
+					$sort = '/' . $val;
+					break;
+				case 'sortparam' :
+					$sortparam = '/' . $val;
+					break;
+				case 'id' :
+					$id = '/' . $val;
+					break;
+				case 'all' :
+					$all = '/' . $val;
+					break;
+			}
+				
+		}
+		
+		if($sort !== NULL && $sortparam === NULL)
+		{
+			$sortparam = '/asc';
+		}
+		
+		$url = $controller . $action . $sort . $sortparam . $page . $id . $all;
+		
+		return $url;
+	}
 		
 }
--- webroot/test.php
+++ webroot/test.php
@@ -2,13 +2,59 @@
 date_default_timezone_set ( 'Europe/Kiev' );
 error_reporting ( E_ALL );
 
-$a = array(
+	$argument = array(
 		
-		'1',
-		'2',
-		'3',
-		'1'
-);
- 
-$b = array_unique($a);
-var_dump($b);
+		'controller' => 'contacts',
+		'action' => 'index',
+		'page' => '1',
+		'sort' => 'first',
+		'sorparam' => 'asc',
+		'id' => '2',
+		'all' => 'all'
+	);
+		
+		$controller = NULL;
+		$action = NULL;
+		$page = NULL;
+		$sort = NULL;
+		$sortparam = NULL;
+		$id = NULL;
+		$all = NULL;
+		
+		foreach ($argument as $key=>$val)
+		{
+			switch ($key)
+			{
+				case 'controller' :
+					$controller = '/' . $val;
+					break;
+				case 'action' :
+					$action = '/' . $val;
+					break;
+				case 'page':
+					$page = '/' . $val;
+					break;
+				case 'sort':
+					$sort = '/' . $val;
+					break;
+				case 'sortparam' :
+					$sortparam = '/' . $val;
+					break;
+				case 'id' :
+					$id = '/' . $val;
+					break;
+				case 'all' :
+					$all = '/' . $val;
+					break;
+			}
+				
+		}
+		
+		if($sort !== NULL && $sortparam === NULL)
+		{
+			$sortparam = '/asc';
+		}
+		
+		$url = $controller . $action . $sort . $sortparam . $page . $id . $all;
+		
+		var_dump($url);
