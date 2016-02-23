22825a5cd38579f2e2b697f8b64d2a772f4fcb3e
Martsynyuk <terr83@gmail.com>
2016-02-23 20:55:57 +0200

    1

--- webroot/js/validation.js
+++ webroot/js/validation.js
@@ -2,7 +2,7 @@
  * 
  */
 	$("input:radio:checked").next('input').attr("onChange", "validation(obj=$(this))");
-	
+
 	function select_telephone_validation(obj)
 	{
 		$('input:radio').next('input').removeAttr("onChange");
@@ -10,7 +10,7 @@
 		$(obj).next('input').attr("onChange", "validation(obj=$(this))");
 		validation($(obj).next('input'));
 	}
-	
+
 	function submit_stop(obj)
 	{
 		$('input[onChange]').each(function(index, value){
@@ -20,6 +20,14 @@
 				$(value).next('div').addClass('no')
 			}
 		});
+		
+		if(date_validation())
+		{
+			$('#valid-year').next('div').removeClass('no').addClass('yes');
+		}
+		else{
+			$('#valid-year').next('div').removeClass('yes').addClass('no');
+		}
 		
 		if($('div').is('.no'))
 			{
@@ -93,7 +101,15 @@
 						someclass = 'no';
 					}
 					break;
-					
+				case 'date':
+					if(date_validation())
+					{
+						someclass = 'yes';
+					}
+					else{
+						someclass = 'no';
+					}
+					break;
 			}
 			
 			if( someclass == 'no')
@@ -108,4 +124,53 @@
 		});
 		
 		obj.next('div').removeClass('yes').removeClass('no').addClass(someclass);
+	}
+	
+	function date_validation()
+	{
+		str = $('#valid-year option').val() + '-'
+				+ $('#valid-month option').val() + '-'
+				+ $('#valid-day option').val();
+		
+		console.log(str);
+		
+		var regular = /^[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}$/i;
+		
+	    if (!regular.test(str))
+	    {
+	    	return false;
+	    }
+	    else{
+				var result = str.split('-');
+				
+		    var y = parseInt(result[0]);
+		    var m = parseInt(result[1]);
+		    var d = parseInt(result[2]);
+
+		    if (m < 1 || m > 12 || y < 1900 || y > 2000)
+		    {
+		    	return false;
+		    }
+		    else{
+			    if (m == 2){
+			    	var days = ((y % 4) == 0) ? 29 : 28;
+			    } 
+			    else if(m == 4 || m == 6 || m == 9 || m == 11){
+			    	var days = 30;
+			    } 
+			    else{
+			    	var days = 31;
+			    }
+			    
+			    if(d >= 1 && d <= days)
+			    {
+			    		
+			    }
+			    else{
+			    	return false;
+			    }
+		    }
+	    }
+	    
+	    return true;
 	}
