b0b634e515f7943934f8b2d5e2abd10945ca1e5e
Martsynyuk <terr83@gmail.com>
2016-03-12 20:28:24 +0200

    1

--- Lib/constant.php
+++ Lib/constant.php
@@ -1,6 +1,6 @@
 <?php
 
-const ROWLIMIT = 5; // number of contact in index page
+const ROWLIMIT = 7; // number of contact in index page
 const SHOWPAGES = 5; // number of displayed pages in pagination
 
 // for conection to DB
