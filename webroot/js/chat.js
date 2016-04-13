b86b6afad3c2783adeecd9b015e2921bed8eaae8
Martsynyuk <terr83@gmail.com>
2016-04-13 17:19:50 +0300

    1

--- webroot/js/chat.js
+++ webroot/js/chat.js
@@ -1,14 +1,22 @@
 
 "use strict";
 
-var chat_body =	'<div class="chat_place chat_open"><div id="chat" class="chat"><span class="header_text">SHOUTBOX</span><form action="/" method="POST"><input class="for_message" type="text" name="message"><input class="submit" type="submit" name="say" value="SAY"><div class="claer"></div></form><div class="message_plase" id="style"></div><span class="load_more">load more</span></div></div>';
-
-document.getElementsByClassName('chat')[0].innerHTML = chat_body;
 var start_count = 5;
 
 class Chat{
-
+	
 	ajax_return(){
+		
+		document.getElementsByClassName("submit")[0].onclick = function(e)
+		{
+			e.preventDefault();
+			chat.send_message();
+		}
+
+		document.getElementsByClassName("load_more")[0].onclick = function()
+		{
+			chat.count_for_old_message();
+		}
 		
 			var xmlhttp;
 	
@@ -21,16 +29,11 @@ class Chat{
 		    }
 	
 		    xmlhttp.onreadystatechange = function() {
-		        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+		        if (xmlhttp.readyState == 4 ) {
 		           if(xmlhttp.status == 200){
 		               document.getElementsByClassName('message_plase')[0].innerHTML = xmlhttp.responseText;
 		           }
-		           else if(xmlhttp.status == 400) {
-		             // alert('There was an error 400')
-		           }
-		           else {
-		               //alert('something else other than 200 was returned')
-		           }
+		           
 		        }
 		    }
 		        
@@ -64,7 +67,7 @@ class Chat{
 		    }
 	
 		    xmlhttp.onreadystatechange = function() {
-		        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+		        if (xmlhttp.readyState == 4 ) {
 		           if(xmlhttp.status == 200){
 		               
 		           }
@@ -108,22 +111,9 @@ class Chat{
 	    return null;
 	}
 	
-	
 }
 
 var chat = new Chat();
-chat.ajax_return();
-
-document.getElementsByClassName("submit")[0].onclick = function(e)
-{
-	e.preventDefault();
-	chat.send_message();
-}
-
-document.getElementsByClassName("load_more")[0].onclick = function()
-{
-	chat.count_for_old_message();
-}
 
 setInterval(function(){ 
 	chat.ajax_return()
