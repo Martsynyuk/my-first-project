fb3f2e2b7a82b69411eed1acca474ac3d10ce562
Martsynyuk <terr83@gmail.com>
2016-05-04 10:50:57 +0300

    test

--- webroot/js/chat_with_jquery.js
+++ webroot/js/chat_with_jquery.js
@@ -1,25 +1,3 @@
-/**
- * 
- * for simple chat only
- * 
- * don't broken my code ^)
- */
-
-$( document ).ready(function() {
-	
-	$('#chat').on('click', '.submit', function(e){
-		chat.send_message();
-		e.preventDefault();
-	});
-	
-	$('#chat').on('click', '.load_more', function(){
-		chat.load_more();
-	});
-	
-	setInterval(function(){ 
-		chat.return_message()
-	}, 1000);  
-});
 
 /**
  * creat new object for chat
@@ -28,125 +6,139 @@ $( document ).ready(function() {
  * 
  */
 
-var chat = {step: 5};
+var chat = {
+	step: 5,
 /**
- * set counter for chat.return_message method
- * 
- */
-
-chat.load_more = function(){
-	
-	var count;
-	
-	if($.cookie('count') == null)
-	{
-		count = chat['step'];
-		chat.set_cookie(count, 1, '/');
-	}
-	else{
-		count = parseInt($.cookie('count')) + chat['step'];
-		chat.set_cookie(count, 1, '/');
-	}
-	
-}
-
-/**
- * 
- * this method return message from data base with ajax query
- * 
- */
-
-chat.return_message = function () {
-
-	var count = '';
-	
-	if($('.cont').data('max') != null && $('.cont').data('min') != null )
-	{
-		chat.date_max = $('.cont').data('max');
-		chat.date_min = $('.cont').data('min');
-	}
-	else{
-		chat.date_max = 0;
-		chat.date_min = 0;
-	}
-	
-	if($.cookie('count') == null)
-	{
-		count = chat['step'];	
-	}
-	else{
-		count = parseInt($.cookie('count'));
-	}
-	
-	if( ! chat.ajax('/contacts/chat_ajax', {'count': count, 'date_max': chat.date_max, 'date_min': chat.date_min}))
-	{	
-		if($('.cont').data('marker') != 0)
-		{	
-			if( chat.data || chat.data != null )
-			{
-				$('.message_plase').empty();
-				$('.message_plase').append(chat.data);
-				chat.data = null;
+* ajax request
+*
+* url - url to action for request
+* data - request for data ( array or string )
+*/
+	ajax: function(url, data){
+		
+		$.ajax({
+			type: 'post',
+			url: url,
+			data: data,
+			response:'json',
+			success: function(data){
+				this.data = data;			
+			},
+			error: function(){
+				console.log('problem')
+				this.data = false;
 			}
-		}
-	}
-};
-
+				
+		});
+		
+	},
 /**
- * 
- * this method send new message to data base with ajax query
- * 
- */
-
-chat.send_message = function () {
-
-	if($('.for_message').val() != '')
-	{
-		if( ! chat.ajax('/contacts/ajax_write_message', {'message': $('.for_message').val()}))
-		{
-			if( chat.data )
-			{
-				$('.for_message').val('');
-			}
-		}
-	}
-};
-
-/**
- * 
  * this method set cookie 
- * 
  * 
  * data - data for set cookie
  * time - time cookie live
  * url - url where cookie is available
  */
+	set_cookie: function (data, time, url){	
+		$.cookie('count', data, {expires: time, path: url});
+	},
+	
+/**
+* this method send new message to data base with ajax query
+*/
 
-chat.set_cookie = function (data, time, url) {	
-	$.cookie('count', data, {expires: time, path: url});
-};
+	send_message: function () {
+
+		if($('.for_message').val() != '')
+		{
+			if( ! this.ajax('/contacts/ajax_write_message', {'message': $('.for_message').val()}))
+			{
+				if( this.data )
+				{
+					$('.for_message').val('');
+				}
+			}
+		}
+	},
 
 /**
- * ajax request
- * 
- * url - url to action for request
- * data - request for data ( array or string )
- */
+* 
+* this method return message from data base with ajax query
+* 
+*/
+	
+	return_message: function () {
 
-chat.ajax = function(url, data){
-  $.ajax({
-		type: 'post',
-		url: url,
-		data: data,
-		response:'json',
-		success: function(data){
-			
-			chat.data = data;			
-		},
-		error: function(){
-			console.log('problem')
-			chat.data = false;
+		var count = '';
+		
+		if($('.cont').data('max') != null && $('.cont').data('min') != null )
+		{
+			this.date_max = $('.cont').data('max');
+			this.date_min = $('.cont').data('min');
+		}
+		else{
+			this.date_max = 0;
+			this.date_min = 0;
 		}
 		
-	});
+		if($.cookie('count') == null)
+		{
+			count = chat['step'];	
+		}
+		else{
+			count = parseInt($.cookie('count'));
+		}
+		
+		if( ! this.ajax('/contacts/chat_ajax', {'count': count, 'date_max': chat.date_max, 'date_min': chat.date_min}))
+		{	
+			if($('.cont').data('marker') != 0)
+			{	
+				if( this.data || this.data != null )
+				{
+					$('.message_plase').empty();
+					$('.message_plase').append(this.data);
+					this.data = null;
+				}
+			}
+		}
+	},
+	
+/**
+* set counter for chat.return_message method
+*  
+*/
+
+	load_more: function(){
+		
+		var count;
+		
+		if($.cookie('count') == null)
+		{
+			count = this.step;
+			this.set_cookie(count, 1, '/');
+		}
+		else{
+			count = parseInt($.cookie('count')) + this.step;
+			this.set_cookie(count, 1, '/');
+		}
+		
+	},
+	
+	ready: $( document ).ready(function() {
+		
+		$('#chat').on('click', '.submit', function(e){
+			chat.send_message();
+			e.preventDefault();
+		});
+		
+		$('#chat').on('click', '.load_more', function(){
+			chat.load_more();
+		});
+		
+		setInterval(function(){ 
+			chat.return_message()
+		}, 1000);  
+		
+	})
 };
+
